a4998352cfb22382523191a42f3195f95f2661fe
Luis Alfredo Toncic <luchis1720@hotmail.com>
2018-09-04 21:24:03 -0300

    clase 5 martes 4-9-18

---
+++ clase 5/Repaso/Repaso/Estante.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repaso
+{
+    public class Estante
+    {
+        #region Fields
+
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        #endregion
+
+        #region Methods
+
+        private Estante (int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante (int capacidad, int ubicacion)
+        : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            int i;
+            StringBuilder sb = new StringBuilder();
+
+            for (i = 0; i < e.productos.Length; i++)
+            {
+                sb.AppendLine(Producto.MostrarProducto(e.productos[i]));
+            }
+
+            return sb.ToString();
+        }
+
+        
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool ans = false;
+            int i;
+
+            for (i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] == p)
+                {
+                    ans = true;
+                    break;
+                }
+            }
+
+            return ans;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            int i;
+
+            if (e == p)
+            {
+                for (i = 0; i < e.productos.Length; i++)
+                {
+                    if (Object.ReferenceEquals(e.productos[i], p))
+                    {
+                        e.productos[i] = null;
+                    }
+                }
+            }
+
+            return e;
+    
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool ans = false;
+            int i;
+            if (e != p)
+            {
+                for (i = 0; i < e.productos.Length; i++)
+                {
+                    if (Object.ReferenceEquals(e.productos[i], null))
+                    {
+                        e.productos[i] = p;
+                    ans = true;
+                    break;
+                    }
+
+                }
+
+            }
+
+            return ans;
+
+        }
+
+        #endregion
+    }
+}
---
+++ clase 5/Repaso/Repaso/Producto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repaso
+{
+    public class Producto
+    {
+        #region Fields
+
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        #endregion
+
+        #region Methods
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            string ans = "";
+
+            ans = p.codigoDeBarra;
+
+            return ans;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (!Object.ReferenceEquals(p, null))
+            {
+                sb.AppendLine("PRODUCTO:");
+                sb.AppendFormat("Marca: {0} ", p.marca);
+                sb.AppendFormat("Codigo de Barra: {0} ", p.codigoDeBarra);
+                sb.AppendFormat("Precio: {0}", p.precio);
+                sb.AppendLine("");
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool ans = false;
+            if (p.marca == marca)
+                ans = true;
+            return ans;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool ans = false;
+            if (!(object.ReferenceEquals(p1, null)) && !(object.ReferenceEquals(p2, null)))
+            {
+                if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                    ans = true;
+            }
+
+            return ans;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        #endregion
+    }
+}
